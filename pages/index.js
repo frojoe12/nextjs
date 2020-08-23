6f082b1cafe18b74b52b68feb2e2f4f8ce1af1f0
frojoe12 <joefrostdesign@gmail.com>
2020-08-23 19:04:50 +0100

    added a dummy json data object for the cards

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
 import './index.css'
 import Card from './Card'
+import React, { Component } from 'react'
+import data from '../data/data.json'
 
-export default () => (
-  <div>
-    <div className="app">
-      <header className="app-header">
-        <img src="/static/logo.png" className="static-logo" alt="logo" />
-      </header>
-      <div className="grid">
-        <Card
-          title={'First item'}
-          text={'This is the text for the first item in the grid.'}
-        />
-        <Card title={'Second item'} />
-        <Card title={'Third item'} />
+class Index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { heading: 'waiting for data to load from database...' }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ heading: 'The Main Heading' })
+    }, 3000)
+  }
+  componentWillUnmount() {}
+  render() {
+    return (
+      <div>
+        <div className="app">
+          <header className="app-header">
+            <img src="/static/logo.png" className="static-logo" alt="logo" />
+          </header>
+          <div className="grid">
+            {data.users.length > 0 ? (
+              data.users.map(({ title, text }) => (
+                <Card key={title} title={title} text={text} />
+              ))
+            ) : (
+              <div>Their are no users in the database.</div>
+            )}
+          </div>
+        </div>
+        <div className="main">
+          <h3>{this.state.heading}</h3>
+          <p>text paragraph</p>
+        </div>
       </div>
-    </div>
-    <div className="main">
-      <h3>Main Heading</h3>
-      <p>text paragraph</p>
-    </div>
-  </div>
-)
+    )
+  }
+}
+
+export default Index
